09c67a7ecd0f9725d3fbdee28c12e7bce67b31a4
infrod <loudassassins@gmail.com>
2026-05-16 21:37:57 -0400

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>Journal de stage</h1>
-      <p>Version test Vercel fonctionnelle.</p>
-    </div>
-  );
-}
